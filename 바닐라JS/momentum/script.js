e72fdd199bbed91735f1a2508ab69290d01ba783
SJUANS <lovejuan95@hanmail.net>
2022-03-09 17:12:44 +0900

    function 연습: 매개변수, 인수, object 안에서 함수 선언과 실행

--- 바닐라JS/momentum/script.js
+++ 바닐라JS/momentum/script.js
@@ -1,60 +1,49 @@
 "use strict";
-// data types: Number(Int, Float), String, Booleans, null, undefined 
-const myID = "PawPaw" // String
-const intNum = 3 // Int
-const floatNum = 3.14 // Float
-
-const empty = null // null: 빈 값을 할당
-let undef 
-console.log(undef); // undefined: 값이 할당되지 않음
- // cf) const로 변수 선언하면 할당 안했을시 error 출력, undefined 값 나오지 않음 
-
-
-// Booleans(불린형)
-const on = true; // 논리 연산자에서 1
-const off = false; // 논리 연산자에서 0
-
-// * true(Booleans) != "true"(String) *
-// * null != "null"(String) *
-
-// --------------------------
-
-// array 연습
-const stack = ["HTML","CSS","Python"]
-console.log(stack);
-
-// array의 특정 값(element) 선택 방법 --> 순서(n번째)로! 
-console.log(stack[0]);
-
-// array에 값 추가
-stack.push("Javasript")
-console.log(stack);
-
-// --------------------------
-
-// object 연습
-const nubDev = {
-    name : "PawPaw", // key : value 구조, key+value = 속성(Property) 
-    level_FE : 2,
-    level_BE : 1,
-    Project_VanilaJS : null,
-    Exp_MakingChallenge : true
+function sayHi(username){ // username 자리(=매개변수, Parameter)엔 임의의 어떤 이름이든 쓰면 된다
+    console.log(username)
 }
-console.log(nubDev);
+sayHi(1);
+sayHi("SJ");
+sayHi(null);  // 1, "JUAN", null 모두 argument
+// 파라메터는 함수의 body({}부분) 바깥에서 접근할 수 없다
 
-// object의 특정 값(value) 선택 방법 2가지 --> key(속성 이름)로!
-console.log(nubDev["name"]); 
-console.log(nubDev.name);
+function yourLevel(username, level){ // 실행시 2개의 argument를 받는 함수(Parameter 2개)
+    console.log(username+"'s level:"+level)
+} 
+yourLevel("Nic", 100); 
+yourLevel("SJ", 3);
+yourLevel("Foo", 25);  
+// 파라메터 여러 개 일시 순서 유의!! (파라메터 순서=인수 순서 되도록)
+// 파라메터와 인수의 개수도 동일하게 맞추기
 
-// console.log(nubDev[0]) --> undefined로 출력됨. 
-// *왜? nubDev는 array가 아니라 object이므로!*
+// 인수를 받지 않은 경우
+function Hi(){
+    console.log("hello");
+}
+Hi("nico");
+Hi("JUAN");
+Hi("Paw"); 
+// 실행 결과: 모두 "hello"로 출력. ()안에 넣은 인수가 아무 영향 x
 
-// (+) 만약 object의 n번째에 있는 key에 접근하고 싶다면
-console.log(Object.keys(nubDev)); //object 안의 키들을 array로 변환
-const fthKey = nubDev[Object.keys(nubDev)[4]];
-console.log(fthKey);
+// 인수를 보내지 않은 경우
+function add(a,b){
+    console.log(a,b)
+}
+add();
+// 실행 결과: undefined 두 개 출력
+// 만약 여기서 a,b->a+b로 바꾸면 NaN 출력 *NaN: Not a Number
 
-// object에 새로운 속성 추가 or 속성값 바꾸기 
-nubDev.Exp_Bootcamp = false // 속성 추가
-nubDev.Project_VanilaJS = "momentum clone" // 이미 있는 속성의 값 변경
-console.log(nubDev);
+// Object 안 function 만들고 실행하기 
+const Hero = {
+    name: "PawPaw",
+    level: 1,
+    skill: function(skilltype){
+        console.log("PawPaw used "+skilltype+"!")
+    } // skill이라는 함수가 Hero의 프로퍼티가 된 것!
+}
+Hero.skill("fireball");
+Hero.skill("defense");
+Hero.skill("Smash");
+
+// Obj.Property 규칙이 Hero.skill과 Console.log에 똑같이 적용되고 있는 것
+// 즉 둘은 동일한 구조이다!
