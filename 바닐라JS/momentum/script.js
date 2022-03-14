e310478f266a67731194ea1bcc5dafca8aa1ed6f
SJUANS <lovejuan95@hanmail.net>
2022-03-14 23:59:04 +0900

    Event 심화(event 기본 정보 object, preventDefault)

--- 바닐라JS/momentum/script.js
+++ 바닐라JS/momentum/script.js
@@ -1,26 +1,13 @@
 "use strict";
-// events와 연동 연습
+const loginForm = document.getElementById("login-form");
+const loginInput = document.querySelector("#login-form input");
+const loginButton = document.querySelector("#login-form button"); 
 
-const Headline = document.querySelector(".foo");
-
-function HeadlineClick(){
-    console.log("You've clicked the Headline!");
+function preventRefresh(event){
+    event.preventDefault();
+    alert(loginInput.value)
+    console.log(event)
 }
 
-Headline.addEventListener("click", HeadlineClick);
-// click 이벤트 발생시에만 HeadlineClick 함수 실행
-// 함수를 인수로 넣을 때 ()쓰지 않도록 유의
-
-function ChangeColor(){
-    Headline.style.color = "blue";  // 선택한 요소 색깔 바꾸기 --> 이 경우 .CSS를 통하지 않고 style을 직접 바꿔준 것 
-}
-function ResetColor(){
-    Headline.style.color = "";  
-}
-
-// event를 주는 두 가지 방법
-Headline.addEventListener("mouseenter", ChangeColor)
-Headline.onmouseleave = ResetColor; // same as: Headline.addEventListener("mouseleave", ResetColor)
-
-
-
+loginForm.addEventListener("submit", preventRefresh)
+// 여기서 의도한 동작을 하려면 object을 loginForm으로 설정해야 함!
