b94a1d2a91cf8c35cd79217a6ba23924f4d56280
SJUANS <lovejuan95@hanmail.net>
2022-04-02 19:17:55 +0900

    함수에 ()붙이지 않았을 때 결과 추가

--- 바닐라JS/Practices/index.js
+++ 바닐라JS/Practices/index.js
@@ -15,4 +15,7 @@ let imFunction2 = function(){
 firstBtn.onclick = imFunction1;
 lastBtn.onclick = imFunction2;
 
-// eventhandler에 등록하는 함수는 ()빼고 쓰자!!!! 
+// eventhandler에 등록하는 함수는 ()빼고 쓰자!!!!
+
+console.log(imFunction1)
+// (이벤트리스너(핸들러)를 등록할 때 제외) 함수 이름 끝에 ()를 붙이지 않으면 함수 그 자체를 반환하게 된다 
