2ce4168b8597b578037f2d6b9e1ad2e49fb2852d
SJUANS <lovejuan95@hanmail.net>
2022-04-15 15:33:01 +0900

    객체의 속성 접근자(dot notation) 연습

--- 바닐라JS/Practices/index.js
+++ 바닐라JS/Practices/index.js
@@ -1,51 +1,21 @@
-//조건문 연습
-function condition1(){
-  let sum = 2+2
-  if (sum === 4){
-    console.log("true")
-  } else {
-    console.log("false")
-  }
+// object의 속성 접근자 중 dot notation 연습 & etc
+
+const cafeMenu = {
+  Americano : 2000,
+  IceLatte : 3000,
+  VanillaLatte : 3500,
+  MintChocoFrappuccino : 4000
 }
 
-condition1();
-
-function condition2(){
-  let sum = 2+2
-  if (sum !== 4){
-    console.log("true")
-  } else {
-    console.log("false")
-  }
+function orderCount(...menus){
+  const yourOrder = menus; // 받은 인수들(menus)는 array 형태로 자동 저장된다
+  let total = 0;
+  yourOrder.forEach(menu => total += menu); // array의 요소들을 모두 더하는 함수 
+  console.log(`It's ${total}won!`)
 }
 
-condition2();
-
-function condition3(){
-  if (3 > 4){
-    console.log("true")
-  } else {
-    console.log("false")
-  }
-}
-
-condition3();
-
-const rightVar = 3;
-let nullVar = null;
-var imVariable;
-
-const varArray = [rightVar, nullVar, imVariable]
-
-function isVariable(){
-  for (i = 0; i < varArray.length; i++){
-    let variable = varArray[i];
-    if (variable){
-      console.log(`No.${i} variable(value:${variable}) is true!`)
-    } else {
-      console.log(`No.${i} variable(value:${variable}) is false.`)
-    }
-  } 
-}
-
-isVariable();
+console.log(cafeMenu.Americano)
+cafeMenu.Americano = 2500; // 기존 프로퍼티의 값 변경
+orderCount(cafeMenu.Americano, cafeMenu.MintChocoFrappuccino);
+cafeMenu.CherryCoke = 3000; // 새로운 프로퍼티를 추가
+orderCount(cafeMenu.CherryCoke, cafeMenu.IceLatte, cafeMenu.VanillaLatte);
